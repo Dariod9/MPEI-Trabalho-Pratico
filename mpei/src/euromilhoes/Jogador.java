d49203c601a8b5a14e25c9b2fbbac500b74412c4
Dário Matos <dario.matos@ua.pt>
2018-11-24 01:12:27 +0000

    Não tinha feito commit da classe Jogador x)

---
+++ mpei/src/euromilhoes/Jogador.java
@@ -0,0 +1,19 @@
+package euromilhoes;
+
+import java.util.Date;
+import java.util.HashMap;
+
+public class Jogador {
+
+	private HashMap<Date,Chave> mapa;
+	private int CC;
+
+	public Jogador(HashMap<Date,Chave> mapa, int CC) {
+		this.mapa=mapa;
+		this.CC=CC;
+		Date a=new Date();
+	
+	}
+	
+	
+}
